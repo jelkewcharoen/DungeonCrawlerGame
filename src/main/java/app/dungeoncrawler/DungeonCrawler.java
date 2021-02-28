e2e98b75fb7572e4694fdb564792572389888b3e
geonhui <glee377@gatech.edu>
2021-02-28 01:34:11 -0500

    glee377 creating DungeonCrawler.java

---
+++ src/main/java/app/dungeoncrawler/DungeonCrawler.java
@@ -0,0 +1,23 @@
+package app.dungeoncrawler;
+
+import app.dungeoncrawler.views.Configuration;
+
+public static class DungeonCrawler {
+
+    private String difficulty;
+    private int money;
+
+    public DungeonCrawler() {
+
+	difficulty = 
+
+
+
+
+    }
+
+
+
+
+
+}
