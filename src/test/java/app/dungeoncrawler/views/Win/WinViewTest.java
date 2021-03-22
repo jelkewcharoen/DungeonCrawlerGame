9ba8e9a5fc029036764b634216d8fe19a1f23194
spillalamarri6 <shar@Sharanyas-MacBook-Air.local>
2021-03-21 22:42:04 -0400

    added win screen test

--- src/test/java/app/dungeoncrawler/views/Win/WinViewTest.java
+++ src/test/java/app/dungeoncrawler/views/Win/WinViewTest.java
@@ -1,3 +1,50 @@
 package app.dungeoncrawler.views.Win;
-public class WinViewTest {
+
+import app.dungeoncrawler.models.Game;
+import app.dungeoncrawler.utils.SceneNames;
+import app.dungeoncrawler.views.AppScenes;
+import app.dungeoncrawler.views.ViewBase;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+import org.testfx.util.WaitForAsyncUtils;
+
+public class WinViewTest extends ApplicationTest {
+    private Stage stage;
+    private AppScenes scene;
+
+    @BeforeClass
+    public static void config() throws Exception {
+        System.getProperties().put("testfx.robot", "glass");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        FxToolkit.hideStage();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        this.scene = new AppScenes(stage);
+        AppScenes.buildScreen(SceneNames.WIN, stage);
+        ViewBase thisScene = this.scene.getScreen(SceneNames.WIN);
+        stage.setScene(thisScene.getScene());
+        stage.setWidth(Game.WINDOW_WIDTH);
+        stage.setHeight(Game.WINDOW_HEIGHT);
+        stage.show();
+        stage.toFront();
+    }
+
+    @Test
+    public void buildSceneTestNavigationToWelcome() {
+        clickOn("#exit");
+        WaitForAsyncUtils.waitForFxEvents();
+        FxAssert.verifyThat("#buttonNavigate", NodeMatchers.isNotNull());
+    }
 }
