60563eaffccb66377eeeed6f08541e89d13d5f40
Ivan Jorge <ivan.jorge001@zoho.com>
2021-04-26 16:37:35 -0400

    test

--- src/main/java/app/dungeoncrawler/models/ShieldPotion.java
+++ src/main/java/app/dungeoncrawler/models/ShieldPotion.java
@@ -11,8 +11,7 @@ public class ShieldPotion extends Potion {
 
     @Override
     public void addToPlayer(Fighter fighter) {
-        Player player = Game.gameSingleInstance().getPlayerI();
-        player.setHealth(player.getHealth().getValue() + 10);
+        fighter.setHealth(fighter.getHealth().getValue() + 10);
         System.out.println("Adding Shield Potion");
     }
 }
